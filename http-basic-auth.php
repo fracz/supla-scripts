3cb7f8b43401764986270720c0a9febcfed06caf
Wojciech Frącz <fraczwojciech@gmail.com>
2017-04-20 18:48:36 +0200

    Disable authorization in CLI

--- http-basic-auth.php
+++ http-basic-auth.php
@@ -1,5 +1,5 @@
 <?php
-if (HTTP_BASIC_USER && (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] !== HTTP_BASIC_USER || $_SERVER['PHP_AUTH_PW'] !== HTTP_BASIC_PASSWORD)) {
+if (php_sapi_name() != "cli" && HTTP_BASIC_USER && (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] !== HTTP_BASIC_USER || $_SERVER['PHP_AUTH_PW'] !== HTTP_BASIC_PASSWORD)) {
     header('WWW-Authenticate: Basic realm="My Realm"');
     header('HTTP/1.0 401 Unauthorized');
     echo 'Authentication required.';
