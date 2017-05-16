b28ba65c59284ade4333351e3e5f40518115f1c2
Wojciech Frącz <fraczwojciech@gmail.com>
2017-05-16 08:44:40 +0200

    Add logging of received voice commands

--- voice/voice.php
+++ voice/voice.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 $config = require __DIR__ . '/config.php';
+define('LOG_FILE', __DIR__ . '/voice.log');
 
 $client = new \SuplaScripts\ConfiguredSuplaApiClient();
 
@@ -8,12 +9,15 @@ $command = $_GET['command'];
 $command = base64_decode($command);
 $command = mb_strtolower($command, 'UTF-8');
 
+file_put_contents(LOG_FILE, PHP_EOL . (new DateTime())->format(DateTime::ATOM) . ': Command: ' . $command, FILE_APPEND);
+
 $actions = 0;
 
 foreach ($config as $cfg) {
     foreach ($cfg['commands'] as $cmd) {
         $cmd = mb_strtolower($cmd, 'UTF-8');
         if (strpos($command, $cmd) !== false) {
+            file_put_contents(LOG_FILE, PHP_EOL . "\tExecuted command: $cmd ($cfg[action])", FILE_APPEND);
             $client->executeCommandsFromString($cfg['action']);
             ++$actions;
             break;
@@ -21,5 +25,6 @@ foreach ($config as $cfg) {
     }
 }
 
-echo $command . PHP_EOL;
-echo "Matched actions: " . $actions;
+file_put_contents(LOG_FILE, PHP_EOL . "\tMatched actions: $actions" . PHP_EOL, FILE_APPEND);
+
+echo 'OK';
