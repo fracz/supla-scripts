8ccfbe4c9998c525fd6118cb70a2bcf5afdcd19a
Wojciech Frącz <fraczwojciech@gmail.com>
2017-05-16 09:45:45 +0200

    Add one directory for logs

--- ConfiguredSuplaApiClient.php
+++ ConfiguredSuplaApiClient.php
@@ -7,11 +7,16 @@ use Supla\ApiClient\SuplaApiClient;
 require __DIR__ . '/config.php';
 require __DIR__ . '/http-basic-auth.php';
 
+define('LOGS_DIR', __DIR__ . '/logs');
+
 header('Content-Type: text/plain; charset=utf-8');
 
 class ConfiguredSuplaApiClient extends SuplaApiClient
 {
-    public function __construct()
+    private $scriptName;
+    private $alreadyLogged = false;
+
+    public function __construct($scriptName = 'notset')
     {
         parent::__construct([
             'server' => SUPLA_SERVER,
@@ -20,6 +25,7 @@ class ConfiguredSuplaApiClient extends SuplaApiClient
             'username' => SUPLA_USERNAME,
             'password' => SUPLA_PASSWORD,
         ]);
+        $this->scriptName = $scriptName;
     }
 
     public function executeCommandFromString($command, $separator = ',')
@@ -34,6 +40,20 @@ class ConfiguredSuplaApiClient extends SuplaApiClient
         $commands = explode($separator, $commands);
         foreach ($commands as $command) {
             $this->executeCommandFromString($command, $commandSeparator);
+        }
+    }
+
+    public function log($entry)
+    {
+        if (!$this->alreadyLogged) {
+            $this->alreadyLogged = true;
+            $entry = PHP_EOL . (new \DateTime())->format(\DateTime::ATOM) . PHP_EOL . "\t" . $entry;
+        } else {
+            $entry = "\t" . $entry;
+        }
+        $wrote = file_put_contents(LOGS_DIR . '/' . $this->scriptName . '.log', PHP_EOL . $entry, FILE_APPEND);
+        if (!$wrote) {
+            echo 'Can not write to log file.' . PHP_EOL;
         }
     }
 }
--- voice/voice.php
+++ voice/voice.php
@@ -1,19 +1,14 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 $config = require __DIR__ . '/config.php';
-define('LOG_FILE', __DIR__ . '/voice.log');
 
-$client = new \SuplaScripts\ConfiguredSuplaApiClient();
+$client = new \SuplaScripts\ConfiguredSuplaApiClient('voice');
 
 $command = $_GET['command'];
 $command = base64_decode($command);
 $command = mb_strtolower($command, 'UTF-8');
 
-$wrote = file_put_contents(LOG_FILE, PHP_EOL . (new DateTime())->format(DateTime::ATOM) . ': Command: ' . $command, FILE_APPEND);
-
-if (!$wrote) {
-    echo 'Can not write to log file.';
-}
+$client->log('Command: ' . $command);
 
 $actions = 0;
 
@@ -21,7 +16,7 @@ foreach ($config as $cfg) {
     foreach ($cfg['commands'] as $cmd) {
         $cmd = mb_strtolower($cmd, 'UTF-8');
         if (strpos($command, $cmd) !== false) {
-            file_put_contents(LOG_FILE, PHP_EOL . "\tExecuted command: $cmd ($cfg[action])", FILE_APPEND);
+            $client->log("Executed command: $cmd ($cfg[action])");
             $client->executeCommandsFromString($cfg['action']);
             ++$actions;
             break;
@@ -29,6 +24,6 @@ foreach ($config as $cfg) {
     }
 }
 
-file_put_contents(LOG_FILE, PHP_EOL . "\tMatched actions: $actions" . PHP_EOL, FILE_APPEND);
+$client->log("Matched actions: $actions");
 
 echo 'OK';
