89931dc4eed4f9c0f50c2f609c98ed2d0c09a8e6
Wojciech Frącz <fraczwojciech@gmail.com>
2017-05-21 20:43:52 +0200

    RGB Remote cleanup

--- rgb/remote.php
+++ rgb/remote.php
@@ -1,17 +1,8 @@
 <!DOCTYPE html>
-<html lang="en" class="no-js">
+<html lang="en">
 <head>
-    <!--		<meta charset="UTF-8" />-->
-    <!--		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> -->
-    <!--		<meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
-    <title>Blueprint: Responsive Full Width Grid</title>
-    <!--		<meta name="description" content="Blueprint: Responsive Full Width Grid Layout" />-->
-    <!--		<meta name="keywords" content="100% grid, layout, columns, images, thumbnails, responsive, full width grid, image grid, css, jquery" />-->
-    <!--		<meta name="author" content="Codrops" />-->
-    <!--		<link rel="shortcut icon" href="../favicon.ico">-->
-    <!--		<link rel="stylesheet" type="text/css" href="css/default.css" />-->
-    <!--		<link rel="stylesheet" type="text/css" href="css/component.css" />-->
-    <!--		<script src="js/modernizr.custom.js"></script>-->
+    <meta charset="UTF-8"/>
+    <title>SUPLA RGB Remote</title>
     <style type="text/css">
         body {
             margin: 0;
@@ -39,7 +30,6 @@ $colors = ['000000', 'ffffff', 'ffff00', 'ff0000', 'ff8800', 'ff00ff', '0000ff',
 </div>
 
 <script>
-
     var anchors = document.getElementsByTagName('a');
     for (var z = 0; z < anchors.length; z++) {
         var elem = anchors[z];
@@ -52,11 +42,6 @@ $colors = ['000000', 'ffffff', 'ffff00', 'ff0000', 'ff8800', 'ff00ff', '0000ff',
         request.open('GET', './rgb.php?channel=<?=$_GET['channel']?>&color=' + color, true);
         request.send();
     }
-
-    //    var request = new XMLHttpRequest();
-    //    request.open('POST', '/my/url', true);
-    //    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    //    request.send(data);
 </script>
 </body>
 </html>
