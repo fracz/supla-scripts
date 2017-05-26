323028d45882ecf76a70060627817364624d1e48
Wojciech Frącz <fraczwojciech@gmail.com>
2017-05-26 08:30:21 +0200

    Improve the temperature script (logs, humidity)

--- temperature/temperature.php
+++ temperature/temperature.php
@@ -2,7 +2,31 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$client = new \SuplaScripts\ConfiguredSuplaApiClient();
+$client = new \SuplaScripts\ConfiguredSuplaApiClient('temperature');
 $channel = $_GET['channel'];
-$temp = $client->channel($channel)->temperature;
-echo number_format($temp, 2) . '°C';
+
+$client->log('#' . $channel);
+
+$response = $client->channel($channel);
+
+if ($response) {
+    $results = [];
+    if (property_exists($response, 'temperature')) {
+        $results[] = number_format($response->temperature, 2) . '°C';
+    }
+    if (property_exists($response, 'humidity')) {
+        $results[] = number_format($response->humidity, 1) . '%';
+    }
+    if (count($results)) {
+        $result = implode(', ', $results);
+    } else {
+        $result = "Channel #$channel seems not to be a thermistor.";
+    }
+
+} else {
+    $result = 'Could not contact the SUPLA API';
+}
+
+$client->log($result);
+echo $result;
+
