6a0616b81358eb7c041ec34613a64ab66a714de3
Wojciech Frącz <fraczwojciech@gmail.com>
2017-05-29 14:59:21 +0200

    Add notification logs

--- notifications/notifications.php
+++ notifications/notifications.php
@@ -6,9 +6,12 @@ $config = require __DIR__ . '/config.php';
 
 $query = $client->readFromGetOrArgv('query');
 
+$client->log('Query: ' . $query);
+
 if (isset($config[$query])) {
     $notificationConfig = $config[$query];
     if (strtolower($_SERVER['REQUEST_METHOD']) != 'PUT') {
+        $client->log('Reading status');
         $channelData = $client->channel($notificationConfig['channel']);
         $notificationNeeded = false;
         foreach ($notificationConfig['expectation'] as $expectedProp => $expectedValue) {
@@ -19,17 +22,24 @@ if (isset($config[$query])) {
                         return $action['label'];
                     }, $notificationConfig['actions']),
                 ]);
+                $client->log(json_encode($notification));
                 echo json_encode($notification);
                 exit;
             }
         }
     } else {
+        $client->log('Executing command');
         $action = file_get_contents('php://input');
         if (isset($notificationConfig['actions'][$action])) {
             if (isset($notificationConfig['actions'][$action]['command'])) {
-                $client->executeCommandsFromString($notificationConfig['actions'][$action]['command']);
+                $command = $notificationConfig['actions'][$action]['command'];
+                $client->executeCommandsFromString($command);
+                $client->log("Executed: " . $command);
+            } else {
+                $client->log("No command defined for " . $action);
             }
         } else {
+            $client->log("Invalid action: " . $action);
             echo "Invalid action: " . $action;
         }
     }
