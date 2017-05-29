58d5d2e81ed11140b419f5722fdb809a75e63001
Wojciech Frącz <fraczwojciech@gmail.com>
2017-05-29 21:41:03 +0200

    Add Automate icons constants

---
+++ utils/AutomateIcons.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SuplaScripts\utils;
+
+class AutomateIcons
+{
+    const ALARM = 0xf0aa;
+    const BULB = 0xf04e;
+    const BULLHORN = 0xf025;
+    const CALENDAR = 0xf033;
+    const CHECK = 0xf0f5;
+    const CROSS = 0xf0f6;
+    const EXCLAMATION = 0xf0ec;
+    const EXCLAMATION_SIGN = 0xf106;
+    const EXCLAMATION_STOP = 0xf0e3;
+    const FLASH = 0xf00d;
+    const HEART = 0xf117;
+    const INFO = 0xf0e9;
+    const KEY = 0xf053;
+    const LOCK_CLOSED = 0xf00b;
+    const LOCK_OPENED = 0xf00b;
+    const MOON = 0xf0b9;
+    const QUESTION_MARK = 0xf0ed;
+    const THERMISTOR = 0xf04f;
+    const TRANSPARENT = 0xf001;
+    const TURN_ON = 0xf0c4;
+    const TURN_ON_ALT = 0xf075;
+    const TURN_ON_ALT2 = 0xf076;
+}
