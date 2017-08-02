69d5b8e96e43337ec04065c447eb43bd9cefc274
Wojciech Frącz <fraczwojciech@gmail.com>
2017-08-02 18:13:39 +0200

    Activating profiles based on time

--- backend/app/commands/DispatchThermostatCommand.php
+++ backend/app/commands/DispatchThermostatCommand.php
@@ -2,6 +2,7 @@
 
 namespace suplascripts\app\commands;
 
+use Cron\CronExpression;
 use suplascripts\models\thermostat\Thermostat;
 use suplascripts\models\thermostat\ThermostatProfile;
 use Symfony\Component\Console\Command\Command;
@@ -22,21 +23,48 @@ class DispatchThermostatCommand extends Command
         $activeThermostats = Thermostat::where([Thermostat::ENABLED => true])->get();
         $output->writeln('<info>Active thermostats: ' . count($activeThermostats) . '</info>');
         foreach ($activeThermostats as $thermostat) {
-            $this->changeProfileIfNeeded($thermostat);
+            $this->changeProfileIfNeeded($thermostat, $output);
         }
     }
 
-    private function changeProfileIfNeeded(Thermostat $thermostat)
+    private function changeProfileIfNeeded(Thermostat $thermostat, OutputInterface $output)
     {
-        if ($thermostat->nextProfileChange->getTimestamp() <= time()) {
+        $now = time();
+        $activeProfile = $thermostat->activeProfile()->first();
+        if ($thermostat->nextProfileChange->getTimestamp() <= $now) {
+            $today = date('Y-m-d', $now);
             foreach ($thermostat->profiles()->get() as $profile) {
                 /** @var ThermostatProfile $profile */
                 if ($profile->activeOn && count($profile->activeOn)) {
                     foreach ($profile->activeOn as $timeSpan) {
-
+                        $weekdays = $timeSpan['weekdays'] ?? [];
+                        $weekdays = count($weekdays) ? implode(',', $weekdays) : '*';
+                        $startTime = ($timeSpan['timeRange'] ?? [])['timeStart'] ?? 0;
+                        $endTime = ($timeSpan['timeRange'] ?? [])['timeEnd'] ?? 1439;
+                        $startTimeMinute = $startTime % 60;
+                        $startTimeHour = floor($startTime / 60);
+                        $endTimeMinute = $endTime % 60;
+                        $endTimeHour = floor($endTime / 60);
+                        $startCronExpression = "$startTimeMinute $startTimeHour * * $weekdays";
+                        $endCronExpression = "$endTimeMinute $endTimeHour * * $weekdays";
+                        $startsToday = CronExpression::factory($startCronExpression)->getNextRunDate($today, 0, true);
+                        $endsToday = CronExpression::factory($endCronExpression)->getNextRunDate($today, 0, true);
+                        if ($startsToday->getTimestamp() <= $now && $endsToday->getTimestamp() >= $now) {
+                            if (!$activeProfile || $activeProfile->id != $profile->id) {
+                                $thermostat->activeProfile()->associate($profile);
+                                $thermostat->save();
+                                $output->writeln('Updated active profile for thermostat ' . $thermostat->id);
+                            }
+                            return;
+                        }
                     }
                 }
             }
+        }
+        if ($activeProfile) {
+            $thermostat->activeProfile()->dissociate();
+            $thermostat->save();
+            $output->writeln('No profile active for thermostat ' . $thermostat->id);
         }
     }
 }
