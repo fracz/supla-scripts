e300f63da52c90a1cab3b9dfcb0cbecef30c3027
Wojciech Frącz <fraczwojciech@gmail.com>
2017-08-20 23:54:55 +0200

    Generate encryption key on init

--- backend/app/commands/InitializeApplicationCommand.php
+++ backend/app/commands/InitializeApplicationCommand.php
@@ -26,6 +26,7 @@ class InitializeApplicationCommand extends Command
             $output->writeln('<error>There is no config.json.</error>');
         } else {
             $this->getApplication()->setAutoExit(false);
+            $this->getApplication()->run(new StringInput('encryptionKey:generate'), $output);
             $this->getApplication()->run(new StringInput('db:migrate'), $output);
         }
     }
