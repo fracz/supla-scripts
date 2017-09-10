56c28aa4c4fe99a4b30ff8c084ba13a48bb98bd5
Wojciech Frącz <fraczwojciech@gmail.com>
2017-09-10 21:42:41 +0200

    Prevent duplicating of crontabs

--- docker/install-crontab.sh
+++ docker/install-crontab.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-(crontab -l; echo ""; cat ./crontab) | crontab
+(crontab -l | grep -q supla-scripts && echo "supla-scripts crontab already installed") || ((crontab -l; echo ""; cat ./crontab) | crontab && echo "supla-scripts crontab has been installed successfully")
