6a903f72386d79e01f263c57f67d8a8b73a19698
Wojciech Frącz <fraczwojciech@gmail.com>
2017-09-23 15:03:48 +0200

    Fix Clients migration

--- backend/database/migrations/20170923145243_clients.php
+++ backend/database/migrations/20170923145243_clients.php
@@ -8,12 +8,12 @@ class Clients extends Migration
 {
     public function change()
     {
-        $this->$this->table(Client::TABLE_NAME)
+        $this->table(Client::TABLE_NAME)
             ->addColumn(Client::LABEL, 'string')
             ->addColumn(Client::ACTIVE, 'boolean')
             ->addColumn(Client::LAST_CONNECTION_DATE, 'timestamp')
             ->addColumn(Client::USER_ID, 'uuid')
-            ->addForeignKey(Client::USER_ID, Client::TABLE_NAME, User::ID, ['delete' => 'CASCADE'])
+            ->addForeignKey(Client::USER_ID, User::TABLE_NAME, User::ID, ['delete' => 'CASCADE'])
             ->addTimestamps(Client::CREATED_AT, Client::UPDATED_AT)
             ->create();
         $this->table(Client::TABLE_NAME)
