ae51c8758b727a35ea3140f8d9d02c8caf7f3de8
Wojciech Frącz <fraczwojciech@gmail.com>
2017-09-24 14:47:31 +0200

    Provide suplascripts.sh script to simplify docker launch

---
+++ docker/suplascripts.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+ln -s ./../var/config/docker-config.env .env >/dev/null 2>&1
+
+if [ ! -f .env ]; then
+  echo "Could not read the docker-config.env configuration file."
+  exit
+fi
+
+export UID=$(id -u) >/dev/null 2>&1
+export GID=$(id -g) >/dev/null 2>&1
+
+source .env >/dev/null 2>&1
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+# remove \r at the end of the env, if exists
+CONTAINER_NAME="$(echo -e "${COMPOSE_PROJECT_NAME}" | sed -e 's/\r$//')"
+
+if [ "$1" = "start" ]; then
+  echo -e "${GREEN}Starting SUPLA scripts containers${NC}"
+
+  if [ "x${PORT_HTTP}${DOMAIN_NAME}" == "x" ] || ( [ "x$PORT_HTTP" != "x" ] && [ "x$DOMAIN_NAME" != "x" ] ); then
+    echo -e "${RED}Could not determine in which mode to start."
+    echo -e "Define either PORT_HTTP or DOMAIN_NAME in .env file to run in STANDALONE or PROXY mode, respectively.${NC}"
+    exit
+  fi
+
+  if [ -z "$PORT_HTTP" ]; then
+    echo -e "${YELLOW}Starting in PROXY mode${NC}"
+    docker-compose -f docker-compose.yml -f docker-compose.letsencrypt.yml up --build -d
+
+  else
+    echo -e "${YELLOW}Starting in STANDALONE mode${NC}"
+    docker-compose -f docker-compose.yml up --build -d
+
+  fi
+
+  sleep 1
+  docker exec -it -u www-data "$CONTAINER_NAME" php supla-scripts init && echo -e "${GREEN}SUPLA Scripts containers has been started.${NC}"
+  ./install-crontab.sh
+
+elif [ "$1" = "stop" ]; then
+  echo -e "${GREEN}Stopping SUPLA Scripts containers${NC}"
+  docker-compose -f docker-compose.yml stop && echo -e "${GREEN}SUPLA Scripts containers has been stopped.${NC}"
+
+elif [ "$1" = "restart" ]; then
+  "./$(basename "$0")" stop
+  sleep 1
+  "./$(basename "$0")" start
+
+else
+  echo -e "${RED}Usage: $0 start|stop|restart${NC}"
+
+fi
--- docs/Installation-docker.md
+++ docs/Installation-docker.md
@@ -10,15 +10,11 @@
 1. `cp var/config/docker-config.env.sample var/config/docker-config.env`
 1. Open the `var/config/docker-config.env` and 
    1. Set the `DATABASE_PASSWORD` to something strong.
-   1. Set the `UID` to your current user's UID acquired with `id -u`.
    1. Set the `PORT_HTTP` and `PORT_HTTPS` to ports on which the application should work.
 1. `cp var/config/config.sample.json var/config/config.json`
 1. Open the `var/config/config.json` and
    1. Set the `db/password` to the same value as in docker configuration.
    1. Change `jwt/key` to something strong.
-1. `ln -s ../var/config/docker-config.env docker/.env`
 1. Put your `server.crt` and `server.key` certificates in `var/ssl` or generate self signed certificates with `var/ssl/generate-self-signed-certs.sh`
-1. `cd docker && docker-compose up --build -d`
-1. `docker exec -u www-data suplascripts php website/supla-scripts init`
+1. `docker/suplascripts.sh start`
 1. Verify that the supla-scripts has started on configured ports.
-1. `docker/install-crontab.sh`
