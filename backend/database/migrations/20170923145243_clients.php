a56ce58879eb934b619d4d0a15ec949e81265903
Wojciech Frącz <fraczwojciech@gmail.com>
2017-10-04 11:18:11 +0200

    Fix broken clients migration

--- backend/database/migrations/20170923145243_clients.php
+++ backend/database/migrations/20170923145243_clients.php
@@ -10,8 +10,8 @@ class Clients extends Migration
     {
         $this->table(Client::TABLE_NAME)
             ->addColumn(Client::LABEL, 'string')
-            ->addColumn(Client::ACTIVE, 'boolean', ['default' => '2017-01-01 00:00:00'])
-            ->addColumn(Client::LAST_CONNECTION_DATE, 'timestamp')
+            ->addColumn(Client::ACTIVE, 'boolean', ['default' =>  true])
+            ->addColumn(Client::LAST_CONNECTION_DATE, 'timestamp', ['default' => '2017-01-01 00:00:00'])
             ->addColumn(Client::USER_ID, 'uuid')
             ->addForeignKey(Client::USER_ID, User::TABLE_NAME, User::ID, ['delete' => 'CASCADE'])
             ->addTimestamps(Client::CREATED_AT, Client::UPDATED_AT)
