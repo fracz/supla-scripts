30e107e485ab3ec174b527d325246eae683e3cb1
Wojciech Frącz <fraczwojciech@gmail.com>
2017-10-11 00:23:05 +0200

    v2.3.0-alpha.2

--- backend/models/voice/FeedbackInterpolator.php
+++ backend/models/voice/FeedbackInterpolator.php
@@ -13,17 +13,6 @@ class FeedbackInterpolator
         return preg_replace_callback('#{{(\d+)\|(on|temperature|humidity|hi)\|(bool|number):?(.+?)?}}#', function ($match) {
             $replacement = $this->replaceChannelState($match[1], $match[2], $match[3], explode(',', $match[4]));
             return $replacement ?: $match[0];
-//            $variable = $match[1];
-//            $resultIndex = isset($match[2]) && $match[2] ? $match[2] : 0;
-//            $value = $results[$resultIndex]->{$variable};
-//            if ($variable == 'on') {
-//                $value = $value ? 'włączone' : 'wyłączone';
-//            } else if ($variable == 'hi') {
-//                $value = $value ? 'zamknięta' : 'otwarta';
-//            } else if (floatval($value)) {
-//                $value = number_format($value, 1, ',', '');
-//            }
-//            return $value;
         }, $feedback);
     }
 
