6b1c02ab208231e49c2a542d17e6d7921d3fd18f
Wojciech Frącz <fraczwojciech@gmail.com>
2017-10-13 08:44:16 +0200

    Allow to save command without feedback

---
+++ backend/database/migrations/20171013084056_voice_commands_nulls.php
@@ -0,0 +1,15 @@
+<?php
+
+use suplascripts\database\migrations\Migration;
+use suplascripts\models\voice\VoiceCommand;
+
+class VoiceCommandsNulls extends Migration
+{
+    public function change()
+    {
+        $this->table(VoiceCommand::TABLE_NAME)
+            ->changeColumn(VoiceCommand::SCENE, 'text', ['null' => true])
+            ->changeColumn(VoiceCommand::FEEDBACK, 'text', ['null' => true])
+            ->update();
+    }
+}
