b9d6848c4bd19ddb29de1c2277cc76222f9ffe15
Wojciech Frącz <fraczwojciech@gmail.com>
2017-10-22 13:31:30 +0200

    Fix init command (auto exit: false)

--- backend/app/commands/InitializeApplicationCommand.php
+++ backend/app/commands/InitializeApplicationCommand.php
@@ -19,11 +19,11 @@ class InitializeApplicationCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->getApplication()->setAutoExit(false);
         $this->getApplication()->run(new StringInput('logo'), $output);
         if (!file_exists(Application::CONFIG_PATH) || !is_readable(Application::CONFIG_PATH)) {
             $output->writeln('<error>There is no config.json.</error>');
         } else {
-            $this->getApplication()->setAutoExit(false);
             $this->getApplication()->run(new StringInput('encryptionKey:generate'), $output);
             $this->getApplication()->run(new StringInput('clear:cache'), $output);
             $this->getApplication()->run(new StringInput('db:migrate'), $output);
