c58e9191eac5d01ae22770843ea69db65e15e751
Wojciech Frącz <fraczwojciech@gmail.com>
2018-01-18 21:38:56 +0100

    Pimp up docker config

--- docker/suplascripts.sh
+++ docker/suplascripts.sh
@@ -2,10 +2,26 @@
 
 cd "$(dirname "$0")"
 
-ln -s ./../var/config/docker-config.env .env >/dev/null 2>&1
-
 if [ ! -f .env ]; then
-  echo "Could not read the docker-config.env configuration file."
+  cd ./../var/config
+  if [ ! -f docker-config.env ]; then
+    cp docker-config.env.sample docker-config.env
+    DB_PASSWORD="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+    sed -i "s+CHANGE_ME_BEFORE_FIRST_LAUNCH+$DB_PASSWORD+g" docker-config.env
+    if [ "$(expr substr $(dpkg --print-architecture) 1 3)" == "arm" ]; then
+      sed -i -E "s/COMPOSE_FILE=(.+)/COMPOSE_FILE=\1:docker-compose.arm32v7.yml/" .env
+    fi
+    if [ ! -f config.json ]; then
+      cp config.sample.json config.json
+      sed -i "s+CHANGE_ME_BEFORE_FIRST_LAUNCH+$DB_PASSWORD+g" config.json
+      SECRET="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+      sed -i "s+CHANGE_SECRET_BEFORE_FIRST_LAUNCH+$SECRET+g" config.json
+    fi
+  fi
+  cd ./../../docker
+  ln -s ./../var/config/docker-config.env .env >/dev/null 2>&1
+  echo -e "${YELLOW}Sample configuration file has been generated for you.${NC}"
+  echo -e "${YELLOW}Please check if the .env file matches your needs and run this command again.${NC}"
   exit
 fi
 
