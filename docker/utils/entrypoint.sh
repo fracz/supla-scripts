0b0295f22c467f78ee4c3549a7c1bbbba660914d
Wojciech Frącz <fraczwojciech@gmail.com>
2018-01-23 22:59:47 +0100

    Fix clear logs path

--- docker/utils/entrypoint.sh
+++ docker/utils/entrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -f /etc/apache2/ssl/server.crt ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/server.key -out /etc/apache2/ssl/server.crt -subj "/C=PL/ST=SUPLA/L=SUPLA/O=SUPLA/CN=SUPLA"
 fi
 
-rm -fr /var/www/supla-scripts/var/cache/*
+rm -fr /var/www/var/cache/*
 /usr/local/bin/php /var/www/supla-scripts init --no-interaction
 
 # first arg is `-f` or `--some-option`
