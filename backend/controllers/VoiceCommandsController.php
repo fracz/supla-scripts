8fbf9190448f0012f22fdf14ae2858d28435f141
Wojciech Frącz <fraczwojciech@gmail.com>
2018-01-28 20:50:28 +0100

    Execute scene if its name is in voice command

--- backend/controllers/VoiceCommandsController.php
+++ backend/controllers/VoiceCommandsController.php
@@ -5,6 +5,7 @@ namespace suplascripts\controllers;
 use Assert\Assertion;
 use suplascripts\models\scene\Scene;
 use suplascripts\models\scene\SceneExecutor;
+use suplascripts\models\User;
 
 class VoiceCommandsController extends BaseController {
 
@@ -22,33 +23,44 @@ class VoiceCommandsController extends BaseController {
         $user = $this->getCurrentUser();
         $user->lastVoiceCommand = $command;
         $user->save();
-        $matchedActions = 0;
         $feedbacks = [];
         $sceneExecutor = new SceneExecutor();
         $user->log('voice', 'Odebrano komendę głosową: ' . $command);
+        $scenesToExecute = $this->findScenesToExecute($user, $command);
+        foreach ($scenesToExecute as $scene) {
+            if ($scene->lastUsed && $scene->lastUsed->getTimestamp() >= time() - 4) {
+                $scene->log('Zignorowano zbyt szybkie wykonanie komendy głosowej: ' . $command);
+            } else {
+                $scene->log('Uruchomiono scenę na podstawie komendy: ' . $command);
+                $feedback = $sceneExecutor->executeWithFeedback($scene);
+                if ($feedback) {
+                    $feedbacks[] = $feedback;
+                }
+            }
+        }
+        return $this->response([
+            'matchedActions' => count($scenesToExecute),
+            'feedback' => implode(PHP_EOL, $feedbacks),
+        ]);
+    }
+
+    /** @return Scene[] */
+    private function findScenesToExecute(User $user, string $command): array {
+        $scenesToExecute = [];
         foreach ($user->scenes as $scene) {
             if ($scene->voiceTriggers) {
                 /** @var Scene $scene */
                 foreach ($scene->voiceTriggers as $trigger) {
                     if (strpos($command, $trigger) !== false) {
-                        if ($scene->lastUsed && $scene->lastUsed->getTimestamp() >= time() - 4) {
-                            $scene->log('Zignorowano zbyt szybkie wykonanie komendy głosowej: ' . $command);
-                            break;
-                        }
-                        ++$matchedActions;
-                        $scene->log('Uruchomiono scenę na podstawie komendy: ' . $command);
-                        $feedback = $sceneExecutor->executeWithFeedback($scene);
-                        if ($feedback) {
-                            $feedbacks[] = $feedback;
-                        }
-                        break;
+                        $scenesToExecute[] = $scene;
+                        continue 2;
                     }
                 }
             }
+            if (strpos($command, $scene->label) !== false) {
+                $scenesToExecute[] = $scene;
+            }
         }
-        return $this->response([
-            'matchedActions' => $matchedActions,
-            'feedback' => implode(PHP_EOL, $feedbacks),
-        ]);
+        return array_unique($scenesToExecute);
     }
 }
