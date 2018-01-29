14e374820b9757eb30febcb9338171ccf487b59a
Wojciech Frącz <fraczwojciech@gmail.com>
2018-01-29 21:20:15 +0100

    chown for var dirs in entrypoint

--- docker/utils/entrypoint.sh
+++ docker/utils/entrypoint.sh
@@ -7,6 +7,7 @@ fi
 
 rm -fr /var/www/var/cache/*
 /usr/local/bin/php /var/www/supla-scripts init --no-interaction
+chown -R www-data:www-data var/backups var/cache var/config var/logs var/ssl var/system
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
