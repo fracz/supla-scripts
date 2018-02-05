bb51ab2963aafbcf51593e9768b52fe9605128b3
Wojciech Frącz <fraczwojciech@gmail.com>
2018-02-05 21:11:30 +0100

    Revert "Execute scene if its name is in voice command"
    This reverts commit 8fbf9190448f0012f22fdf14ae2858d28435f141.

--- backend/controllers/VoiceCommandsController.php
+++ backend/controllers/VoiceCommandsController.php
@@ -5,7 +5,6 @@ namespace suplascripts\controllers;
 use Assert\Assertion;
 use suplascripts\models\scene\Scene;
 use suplascripts\models\scene\SceneExecutor;
-use suplascripts\models\User;
 
 class VoiceCommandsController extends BaseController {
 
@@ -23,44 +22,33 @@ class VoiceCommandsController extends BaseController {
         $user = $this->getCurrentUser();
         $user->lastVoiceCommand = $command;
         $user->save();
+        $matchedActions = 0;
         $feedbacks = [];
         $sceneExecutor = new SceneExecutor();
         $user->log('voice', 'Odebrano komendę głosową: ' . $command);
-        $scenesToExecute = $this->findScenesToExecute($user, $command);
-        foreach ($scenesToExecute as $scene) {
-            if ($scene->lastUsed && $scene->lastUsed->getTimestamp() >= time() - 4) {
-                $scene->log('Zignorowano zbyt szybkie wykonanie komendy głosowej: ' . $command);
-            } else {
-                $scene->log('Uruchomiono scenę na podstawie komendy: ' . $command);
-                $feedback = $sceneExecutor->executeWithFeedback($scene);
-                if ($feedback) {
-                    $feedbacks[] = $feedback;
-                }
-            }
-        }
-        return $this->response([
-            'matchedActions' => count($scenesToExecute),
-            'feedback' => implode(PHP_EOL, $feedbacks),
-        ]);
-    }
-
-    /** @return Scene[] */
-    private function findScenesToExecute(User $user, string $command): array {
-        $scenesToExecute = [];
         foreach ($user->scenes as $scene) {
             if ($scene->voiceTriggers) {
                 /** @var Scene $scene */
                 foreach ($scene->voiceTriggers as $trigger) {
                     if (strpos($command, $trigger) !== false) {
-                        $scenesToExecute[] = $scene;
-                        continue 2;
+                        if ($scene->lastUsed && $scene->lastUsed->getTimestamp() >= time() - 4) {
+                            $scene->log('Zignorowano zbyt szybkie wykonanie komendy głosowej: ' . $command);
+                            break;
+                        }
+                        ++$matchedActions;
+                        $scene->log('Uruchomiono scenę na podstawie komendy: ' . $command);
+                        $feedback = $sceneExecutor->executeWithFeedback($scene);
+                        if ($feedback) {
+                            $feedbacks[] = $feedback;
+                        }
+                        break;
                     }
                 }
             }
-            if (strpos($command, $scene->label) !== false) {
-                $scenesToExecute[] = $scene;
-            }
         }
-        return array_unique($scenesToExecute);
+        return $this->response([
+            'matchedActions' => $matchedActions,
+            'feedback' => implode(PHP_EOL, $feedbacks),
+        ]);
     }
 }
