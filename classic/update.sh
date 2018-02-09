57dacf9a1d0e94ef471669d54461d302f5a35851
Michał Wieczorek <michael10@o2.pl>
2018-02-09 11:08:26 +0100

    Update update.sh

--- classic/update.sh
+++ classic/update.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
 
-##
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-#
-# @author Michał Wieczorek @michael
-##
-
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root. For example sudo ./$(basename "$0") 2.7.3" 1>&2
    exit 1
