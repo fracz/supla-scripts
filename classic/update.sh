0d77b899239b9fad5a8f04d59dd4803b576d4db6
Michał Wieczorek <michael10@o2.pl>
2019-04-02 12:18:51 +0200

    Automatic update script

--- classic/update.sh
+++ classic/update.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
 
 if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root. For example sudo ./$(basename "$0") 2.7.3" 1>&2
+   echo "This script must be run as root. For example sudo ./$(basename "$0")" 1>&2
    exit 1
 
 fi
 
-if [ -z "$*" ]; then
-   echo "Please add version to update. For example sudo ./$(basename "$0") 2.7.3" 1>&2
-   exit 1
-fi
+echo "Updating Supla-Scripts. Please wait..."
 
-wget https://github.com/fracz/supla-scripts/releases/download/v$1/supla-scripts-$1.tar.gz
+get_latest_release_supla_scripts() {
+  curl --silent "https://api.github.com/repos/fracz/supla-scripts/releases/latest" |
+    grep '"tag_name":' |
+    sed -E 's/^.*\"v//' |
+    sed -E 's/\".*$//'
+}
 
-tar -zxf supla-scripts-$1.tar.gz -C /var/www/supla-scripts
+SUPLASCRIPTS=`get_latest_release_supla_scripts`
+
+wget https://github.com/fracz/supla-scripts/releases/download/v$SUPLASCRIPTS/supla-scripts-$SUPLASCRIPTS.tar.gz
+
+tar -zxf supla-scripts-$SUPLASCRIPTS.tar.gz -C /var/www/supla-scripts
 
 php /var/www/supla-scripts/supla-scripts init
 
 chown -R www-data:www-data /var/www/supla-scripts
 
-echo "FINISH!"
+rm -fr supla-scripts-$SUPLASCRIPTS.tar.gz
 
+echo "FINISH!"
