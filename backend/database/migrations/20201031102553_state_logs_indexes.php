2f3689bbf03ab209e615c1463623696761d79b4d
Wojciech Frącz <fraczwojciech@gmail.com>
2020-10-31 11:09:10 +0100

    Add proper indexes to state logs

---
+++ backend/database/migrations/20201031102553_state_logs_indexes.php
@@ -0,0 +1,13 @@
+<?php
+
+use suplascripts\database\migrations\Migration;
+use suplascripts\models\log\StateLogEntry;
+
+class StateLogsIndexes extends Migration {
+    public function change() {
+        $this->table(StateLogEntry::TABLE_NAME)
+            ->addIndex([StateLogEntry::USER_ID, StateLogEntry::CREATED_AT])
+            ->addIndex([StateLogEntry::USER_ID, StateLogEntry::CHANNEL_ID])
+            ->update();
+    }
+}
