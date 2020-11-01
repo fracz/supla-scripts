1d18ad6d61312b34573c28b52b4e94d196a78b5a
Wojciech Frącz <fraczwojciech@gmail.com>
2020-11-01 09:35:21 +0100

    Add index names so raspberry PI does not complain

--- backend/database/migrations/20201031102553_state_logs_indexes.php
+++ backend/database/migrations/20201031102553_state_logs_indexes.php
@@ -6,9 +6,9 @@ use suplascripts\models\log\StateLogEntry;
 class StateLogsIndexes extends Migration {
     public function change() {
         $this->table(StateLogEntry::TABLE_NAME)
-            ->addIndex([StateLogEntry::USER_ID, StateLogEntry::CREATED_AT])
-            ->addIndex([StateLogEntry::USER_ID, StateLogEntry::CHANNEL_ID])
-            ->addIndex([StateLogEntry::CREATED_AT])
+            ->addIndex([StateLogEntry::USER_ID, StateLogEntry::CREATED_AT], ['name' => 'state_logs_user_id_created_at_idx'])
+            ->addIndex([StateLogEntry::USER_ID, StateLogEntry::CHANNEL_ID], ['name' => 'state_logs_user_id_channel_id_idx'])
+            ->addIndex([StateLogEntry::CREATED_AT], ['name' => 'state_logs_created_at_idx'])
             ->update();
     }
 }
