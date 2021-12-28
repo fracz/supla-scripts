558b9a2347730b26fbf1cfcfdce5e95534b45723
Wojciech Frącz <fraczwojciech@gmail.com>
2021-12-28 16:50:42 +0100

    Fix quotation helper

--- public/helpers/zamyslenie.php
+++ public/helpers/zamyslenie.php
@@ -1,4 +1,4 @@
 <?php
 $html = file_get_contents('https://www.zamyslenie.pl/aforyzm');
-preg_match('#<blockquote.+?<h2.+?>(.+)</h2>#s', $html, $match);
+preg_match('#class="quote-single.+?>(.+?)</p>#s', $html, $match);
 echo $match[1];
