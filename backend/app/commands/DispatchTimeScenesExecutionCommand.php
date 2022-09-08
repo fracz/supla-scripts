de0f649d403f579eda26a2e1296ffef1aa568c7e
Wojciech Frącz <fraczwojciech@gmail.com>
2022-09-08 13:47:07 +0200

    Add verbose logs to DispatchTimeScenesExecutionCommand

--- backend/app/commands/DispatchTimeScenesExecutionCommand.php
+++ backend/app/commands/DispatchTimeScenesExecutionCommand.php
@@ -9,6 +9,7 @@ use suplascripts\models\scene\SceneExecutor;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class DispatchTimeScenesExecutionCommand extends Command {
 
@@ -23,14 +24,28 @@ class DispatchTimeScenesExecutionCommand extends Command {
         $now = new \DateTime('now', new \DateTimeZone('UTC'));
         $sceneExecutor = new SceneExecutor();
         $pendingScenesQuery = PendingScene::where(PendingScene::EXECUTE_AFTER, '<=', $now);
-        if ($pendingScenesQuery->count()) {
+        $io = new SymfonyStyle($input, $output);
+        $count = $pendingScenesQuery->count();
+        if ($input->isInteractive()) {
+            $io->writeln('Pending scenes count (for now): ' . $count);
+        }
+        if ($count) {
             $pendingScenes = $pendingScenesQuery->get();
             $pendingScenesQuery->delete();
             foreach ($pendingScenes as $pendingScene) {
-                /** @var PendingScene $pendingScene */
-                $container = Application::getInstance()->getContainer();
-                $container['currentUser'] = $pendingScene->scene->user;
-                $sceneExecutor->executePendingScene($pendingScene);
+                try {
+                    if ($io->isVerbose() || $io->isVeryVerbose()) {
+                        $io->writeln('Scene: ' . $pendingScene->scene->id);
+                    }
+                    /** @var PendingScene $pendingScene */
+                    $container = Application::getInstance()->getContainer();
+                    $container['currentUser'] = $pendingScene->scene->user;
+                    $sceneExecutor->executePendingScene($pendingScene);
+                } catch (\Exception $e) {
+                    if ($input->isInteractive()) {
+                        $io->error($e->getMessage());
+                    }
+                }
             }
         }
     }
