27ec149ee63af659b3ece365e8b0dc79e49541c3
Wojciech Frącz <fraczwojciech@gmail.com>
2024-04-29 13:08:14 +0200

    Parse integer value of page param in logs

--- backend/controllers/LogsController.php
+++ backend/controllers/LogsController.php
@@ -9,7 +9,7 @@ class LogsController extends BaseController {
     public function getLatestAction() {
         $limit = min(100, intval($this->request()->getParam('limit', 100)));
         $entityId = $this->request()->getParam('entityId', null);
-        $page = $this->request()->getParam('page', 0);
+        $page = intval($this->request()->getParam('page', 0));
         $filters = [LogEntry::USER_ID => $this->getCurrentUser()->id];
         if ($entityId) {
             $filters[LogEntry::ENTITY_ID] = $entityId;
